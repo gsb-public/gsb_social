dcbfb9f764bd81996d9490bf750ad92ec05fe3c1
Greg Mercer (GBear) <gmercer@stanford.edu>
2014-09-26 13:30:38 -0700

    WP-4352 Moving code from here to gsb_feature_google_analytics_settings

--- js/gsb_social.js
+++ js/gsb_social.js
@@ -3,9 +3,7 @@
   Drupal.behaviors.gsb_social = {
     attach: function (context, settings) {
       $(document).ready(function() {
-        
         // config the social sharrre buttons
-
         $('.sharrre-twitter').sharrre({
           share: {
             twitter: true
@@ -53,60 +51,6 @@
             api.openPopup('linkedin');
           }
         });
-        
-        // config scroll depth 
-
-        if ($.scrollDepth) {
-          $.scrollDepth({
-            minHeight: 2000,
-          });
-        }
-
-        // config riveted (time tracking)
-
-        if (riveted) {
-          riveted.init({
-            reportInterval: 10,  // Default: 5
-            idleTimeout: 20,      // Default: 30
-            nonInteraction: true  // Default: true
-          });
-        }        
-
-        // config event clicks
-
-        // Top Stories
-        $(".content .node-idea-story .group-right .top-stories-wrapper a").click(function() {
-          alert('click top story');
-        });        
-
-        // Explore More
-        $(".content .node-idea-story .group-left .explore-more-content .field-name-title a").click(function() {
-          alert('click explore more');
-        });        
-
-        // Related Content
-        $(".content .node-idea-story .group-right .field-name-field-related-faculty a").click(function() {
-          alert('click related content faculty');
-        });        
-
-        $(".content .node-idea-story .group-right .field-name-field-related-idea-story a").click(function() {
-          alert('click related content idea story');
-        });        
-
-        $(".content .node-idea-story .group-right .field-collection-item-field-related-other-unlimited a").click(function() {
-          alert('click related content other');
-        });        
-
-        // Top Key Taxonomy Terms
-        $(".content .node-idea-story .group-left .group_wrapper_header .field-name-field-key-taxonomy a").click(function() {
-          alert('click top key taxonomy');
-        });        
-
-        // Bottom Taxonomy
-        $(".content .node-idea-story .group-left .group_wrapper_taxonomy a").click(function() {
-          alert('click bottom key taxonomy');
-        });        
-
       }); // end document.ready 
     } // end attach
   }; // end Drupal.behaviors.gsb_social
