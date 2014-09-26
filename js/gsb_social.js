80ab8115df56cc1c7a0cd852527700bd08e8d5ac
Greg Mercer <gmercer@stanford.edu>
2014-09-26 11:29:22 -0700

    WP-4352 Added stubbed/testing code for handling idea story events

--- js/gsb_social.js
+++ js/gsb_social.js
@@ -3,7 +3,9 @@
   Drupal.behaviors.gsb_social = {
     attach: function (context, settings) {
       $(document).ready(function() {
+        
         // config the social sharrre buttons
+
         $('.sharrre-twitter').sharrre({
           share: {
             twitter: true
@@ -50,9 +52,61 @@
             api.simulateClick('sharrre-linkedin');
             api.openPopup('linkedin');
           }
-        }); 
+        });
+        
+        // config scroll depth 
 
-      
+        if ($.scrollDepth) {
+          $.scrollDepth({
+            minHeight: 2000,
+          });
+        }
+
+        // config riveted (time tracking)
+
+        if (riveted) {
+          riveted.init({
+            reportInterval: 10,  // Default: 5
+            idleTimeout: 20,      // Default: 30
+            nonInteraction: true  // Default: true
+          });
+        }        
+
+        // config event clicks
+
+        // Top Stories
+        jQuery(".content .node-idea-story .group-right .top-stories-wrapper a").click(function() {
+          alert('click top story');
+        });        
+
+        // Explore More
+        jQuery(".content .node-idea-story .group-left .explore-more-content .field-name-title a").click(function() {
+          alert('click explore more');
+        });        
+
+        // Related Content
+        jQuery(".content .node-idea-story .group-right .field-name-field-related-faculty a").click(function() {
+          alert('click related content faculty');
+        });        
+
+        jQuery(".content .node-idea-story .group-right .field-name-field-related-idea-story a").click(function() {
+          alert('click related content idea story');
+        });        
+
+        jQuery(".content .node-idea-story .group-right .field-collection-item-field-related-other-unlimited a").click(function() {
+          alert('click related content other');
+        });        
+
+        // Top Key Taxonomy Terms
+        jQuery(".content .node-idea-story .group-left .group_wrapper_header .field-name-field-key-taxonomy a").click(function() {
+          alert('click top key taxonomy');
+        });        
+
+        // Bottom Taxonomy
+        jQuery(".content .node-idea-story .group-left .group_wrapper_taxonomy a").click(function() {
+          alert('click bottom key taxonomy');
+        });        
+
       }); // end document.ready 
     } // end attach
   }; // end Drupal.behaviors.gsb_social
