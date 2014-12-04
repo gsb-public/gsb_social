544ea668c609f93a9a9f605ab837049776fb53cb
Adam Moore <redndahead@gmail.com>
2014-12-04 07:56:57 -0800

    Use an each so our code runs independently on each set of buttons.

--- js/gsb_social.js
+++ js/gsb_social.js
@@ -12,52 +12,56 @@
         // Force protocol to http.
         newurl = newurl.replace(/.*?:\/\//g, "http://");
 
-        $('.sharrre-twitter').sharrre({
-          share: {
-            twitter: true
-          },
-          enableHover: false,
-          enableTracking: true,
-          buttons: { twitter: { via: 'stanfordbiz', url: newurl } },
-          click: function(api, options){
-            api.simulateClick('sharrre-twitter');
-            api.openPopup('twitter');
-          }
-        });
-        $('.sharrre-facebook').sharrre({
-          share: {
-            facebook: true
-          },
-          enableHover: false,
-          enableTracking: true,
-          buttons: { facebook: {action: 'share', title: 'Share'}},  
-          click: function(api, options){
-            api.simulateClick('sharrre-facebook');
-            api.openPopup('facebook');
-          }
-        });
-        $('.sharrre-googleplus').sharrre({
-          share: {
-            googlePlus: true
-          },
-          urlCurl: '/gsb-social/sharrre/google-plus-count',
-          enableHover: false,
-          enableTracking: true,
-          click: function(api, options){
-            api.simulateClick('sharrre-googleplus');
-            api.openPopup('googlePlus');
-          }
-        }); 
-        $('.sharrre-linkedin').sharrre({
-          share: {
-            linkedin: true
-          },
-          enableHover: false,
-          enableTracking: true,
-          click: function(api, options){
-            api.simulateClick('sharrre-linkedin');
-            api.openPopup('linkedin');
-          }
+        $('.gsb-social-buttons').each(function() {
+          var $buttons = $(this);
+
+          $buttons.find('.sharrre-twitter').sharrre({
+            share: {
+              twitter: true
+            },
+            enableHover: false,
+            enableTracking: true,
+            buttons: { twitter: { via: 'stanfordbiz', url: newurl } },
+            click: function(api, options){
+              api.simulateClick('sharrre-twitter');
+              api.openPopup('twitter');
+            }
+          });
+          $buttons.find('.sharrre-facebook').sharrre({
+            share: {
+              facebook: true
+            },
+            enableHover: false,
+            enableTracking: true,
+            buttons: { facebook: {action: 'share', title: 'Share'}},
+            click: function(api, options){
+              api.simulateClick('sharrre-facebook');
+              api.openPopup('facebook');
+            }
+          });
+          $buttons.find('.sharrre-googleplus').sharrre({
+            share: {
+              googlePlus: true
+            },
+            urlCurl: '/gsb-social/sharrre/google-plus-count',
+            enableHover: false,
+            enableTracking: true,
+            click: function(api, options){
+              api.simulateClick('sharrre-googleplus');
+              api.openPopup('googlePlus');
+            }
+          });
+          $buttons.find('.sharrre-linkedin').sharrre({
+            share: {
+              linkedin: true
+            },
+            enableHover: false,
+            enableTracking: true,
+            click: function(api, options){
+              api.simulateClick('sharrre-linkedin');
+              api.openPopup('linkedin');
+            }
+          });
         });
       }); // end document.ready 
     } // end attach
